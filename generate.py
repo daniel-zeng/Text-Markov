76240515492e86a3638b5b38afc28f38adfc26a4
Daniel <danielzeng@berkeley.edu>
2018-07-28 21:32:43 -0700

    update model

--- generate.py
+++ generate.py
@@ -1,8 +1,8 @@
 import glob, re
 from numpy.random import choice
 
-def load_texts(directory):
-	return glob.glob(directory + "\\*.txt")
+def load_texts():
+	return glob.glob("dataset\\*.txt")
 
 def has_alpha(word):
 	return re.search('[a-zA-Z]', word) is not None
@@ -13,6 +13,10 @@ def has_ending(word):
 def only_alphanum(word):
 	return re.sub(r'[^a-zA-Z0-9]', '', word)
 
+def process_key(word1, word2):
+	seperate_char = "^^"
+	return (only_alphanum(word1) + seperate_char + only_alphanum(word2)).lower()
+
 word2list = {}
 word_pair_freq = {}
 word2sample = {}
@@ -20,30 +24,34 @@ word2sample = {}
 def process_text(file):
 	with open(file) as f:
 		words = f.read().split()
+		prev_word2 = ""
 		prev_word = "."
 		for word in words:
 			if not has_alpha(word):
 				continue
 			else:
-				curr = word.lower()
+				curr = word
 				if has_ending(word):
 					no_period = curr.replace(".", "").replace("?", "")
-					process_word(prev_word, no_period)
-					process_word(only_alphanum(no_period), ".")
+					assert len(no_period) > 0
+					process_word(prev_word2, prev_word, no_period)
+					process_word(prev_word, no_period, ".")
+					prev_word2 = ""
 					prev_word = "."
 				else:
-					process_word(prev_word, curr)
-					prev_word = only_alphanum(curr)
+					process_word(prev_word2, prev_word, curr)
+					prev_word2, prev_word = prev_word, curr
 		print(len(words))
 
-def process_word(prev, curr):
-	if prev not in word2list:
-		word2list[prev] = set()
-	word2list[prev].add(curr)
-	if (prev, curr) not in word_pair_freq:
-		word_pair_freq[(prev, curr)] = 1
+def process_word(prev2, prev1, target):
+	key = process_key(prev2, prev1)
+	if key not in word2list:
+		word2list[key] = set()
+	word2list[key].add(target)
+	if (key, target) not in word_pair_freq:
+		word_pair_freq[(key, target)] = 1
 	else:
-		word_pair_freq[(prev, curr)] += 1
+		word_pair_freq[(key, target)] += 1
 
 def get_prob(val, temp):
 	if temp < 1e-2:
@@ -64,7 +72,7 @@ class WordSampler:
 		return choice(self.words, p=self.freqs)
 
 temp = 1
-def create_samplers():
+def create_sampler():
 	for key, word_set in word2list.items():
 		words = []
 		freqs_unnorm = []
@@ -78,23 +86,23 @@ def create_samplers():
 		word2sample[key] = WordSampler(words, freqs)
 		
 
-def generate(start, num_words = 1000):
+def generate(start0, start, num_words = 1000):
+	curr0 = start0
 	curr = start
 	for i in range(num_words):
-		curr = word2sample[curr].sample()
-		if curr != ".":
-			print("", curr, end = "")
-			curr = only_alphanum(curr)
+		new_word = word2sample[process_key(curr0, curr)].sample()
+		curr0, curr = curr, new_word
+
+		
+		if curr == ".":
+			curr0 = ""
+			curr = "."
+			print(curr, end = "") 
 		else:
-			print(curr, end = "")
-			
-
-
+			print(" " + curr, end = "") 
 if __name__ == '__main__':
-	directory = "dataset"
-	for text in load_texts(directory):
+	for text in load_texts():
 		process_text(text)
-	create_samplers()
-	start = "."
-	generate(start)
+	create_sampler()
+	generate("", ".")
 	print()
